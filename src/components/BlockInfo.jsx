6428b590787114a7eb883ad7133a6b2d39306bfd
namdar12 <namdar.mesri12@gmail.com>
2023-01-27 23:58:44 -0800

    more CSS

--- src/components/BlockInfo.jsx
+++ src/components/BlockInfo.jsx
@@ -15,32 +15,27 @@ function BlockInfo({block}) {
   const handleClick = () => {
 
    history.push("/transaction")
-  //   {
-  //     pathname: "/transaction",
-  //     state: { prop: blockNumber }
-  // });
+
   }
-  //to={{pathname: '/transaction', state: { blockNumber: blockNumber }}} 
+
     return (
     <Link to={{pathname: '/transaction', state: { blockNumber: blockNumber }}} onClick={() => handleClick()}>
       <div className="block-info">
-        <div className="block-info__nonce">{block.nonce}</div>
-        <div className="block-info__hash">{block.hash}</div>
-        <div className="block-info__number">{block.number}</div>
         <div>Nonce: {block.nonce}</div>
+        <br></br>
         <div>Hash: {block.hash}</div>
+        <br></br>
         <div>Number: {block.number}</div>
+        <br></br>
         <div>Parent Hash: {block.parentHash}</div>
-        <div>SHA3 Uncles: {block.sha3Uncles}</div>
-        <div>Logs Bloom: {block.logsBloom}</div>
+        <br></br>
         <div>Transactions Root: {block.transactions ? block.transactions[0] : ''}</div>
+        <br></br>
         <div>Transactions Root: {block.transactions ? block.transactions[1] : ''}</div>
+        <br></br>
         <div>Transactions Root: {block.transactions ? block.transactions[2] : ''}</div>
-        <div>State Root: {block.stateRoot}</div>
-        <div>Receipts Root: {block.receiptsRoot}</div>
+        <br></br>
         <div>Miner: {block.miner}</div>
-        <div>Difficulty: {block.difficulty}</div>
-        <div>Total Difficulty: {block.totalDifficulty}</div>
       </div>
       </Link>
     );
